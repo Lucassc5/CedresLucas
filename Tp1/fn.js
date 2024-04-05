0e9cce1bc2e83993fe7edf2c4dadefd77204a9fe
Cedres Lucas <lu793ca@gmail.com>
2024-04-05 20:59:19 -0300

    TP1

---
+++ Tp1/fn.js
@@ -0,0 +1,16 @@
+/*$array = ['pera', 'manzana', 'frutilla', 'naranja', 'mandarina'];
+
+for (i = 0; i < $array.length; i++) {
+console.log ($array [i]);
+} */
+
+/*let frutas = ['pera', 'manzana', 'frutilla', 'naranja', 'mandarina'];
+/*frutas.forEach (fruta => {                                  //recorre todo el array
+    console.log (fruta);
+}); */
+
+/*for (const fruta of frutas) {
+    if (fruta == 'manzana') {               //si pongo = asigno, pero si pongo == comparo
+        console.log (fruta);
+    }
+}
