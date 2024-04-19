902ca21188b0d3709f992ca02ef01c0fecac3ba8
Cedres Lucas <lu793ca@gmail.com>
2024-04-19 16:27:47 -0300

    TP3

--- TP3/fn.js
+++ TP3/fn.js
@@ -1,6 +1,18 @@
-function usuario() {
-    fetch ('https://randomuser.me/api/${per}');
-    .then (res => res.json());
-    .then (data => data)
-    data
+function fetchRandomUser() {
+    fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then(data => {
+            const user = data.results[0];
+            const userInfo = `
+                <img src="${user.picture.large}" alt="${user.name.first} ${user.name.last}">
+                <p><h1>Nombre:</h1> ${user.name.title} ${user.name.first} ${user.name.last}</p>
+                <p><h1>Género:</h1> ${user.gender}</p>
+                <p><h1>coordinates:</h1> ${user.coordinates},${user.coordinates}</p>
+                
+            `;
+            document.getElementById('user-info').innerHTML = userInfo;
+        })
+        .catch(error => {
+            console.error('Error', error);
+        });  
 }
