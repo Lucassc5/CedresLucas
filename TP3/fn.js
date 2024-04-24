2d96805aa2e6e397a1245acfde10f9f29d922c83
Cedres Lucas <lu793ca@gmail.com>
2024-04-24 17:27:25 -0300

    tp3

--- TP3/fn.js
+++ TP3/fn.js
@@ -1,18 +1,33 @@
-function fetchRandomUser() {
-    fetch('https://randomuser.me/api/')
-        .then(response => response.json())
+function buscarusuario() {
+    const URL = 'https://randomuser.me/api/'
+    fetch (URL)
+        .then(res => res.json())
         .then(data => {
-            const user = data.results[0];
-            const userInfo = `
-                <img src="${user.picture.large}" alt="${user.name.first} ${user.name.last}">
-                <p><h1>Nombre:</h1> ${user.name.title} ${user.name.first} ${user.name.last}</p>
-                <p><h1>Género:</h1> ${user.gender}</p>
-                <p><h1>coordinates:</h1> ${user.coordinates},${user.coordinates}</p>
-                
+
+            const userInfo = document.getElementById('userInfo');
+            console.log(data);
+
+            userInfo.innerHTML= `
+                <p><h1>Nombre:</h1>${data.results[0].name.first} ${data.results[0].name.last}</p>
+                <img src="${data.results[0].picture.large}">
+                <p><h1>Latitude:</h1> ${data.results[0].location.coordinates.latitude}</p>
+                <p><h1>Longitude:</h1>${data.results[0].location.coordinates.longitude}</p>
             `;
+            map(data.results[0].location.coordinates.latitude, data.results[0].location.coordinates.longitude);
             document.getElementById('user-info').innerHTML = userInfo;
         })
         .catch(error => {
             console.error('Error', error);
-        });  
+        });
 }
+
+
+function map(latitude,longitude) {
+
+    var map = L.map('map').setView([latitude,longitude], 13);
+    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+} 
